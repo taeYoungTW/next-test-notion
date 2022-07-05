e0da60e484e34bae161596978571bc01ef579590
TaeYoung Kim <taezero@twinny.ai>
2022-07-05 19:29:31 +0900

    CSR with notion API

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,24 +54,9 @@ const Home: NextPage = () => {
 
     useEffect(() => {
         (async () => {
-            const notion = new Client({
-                auth: process.env.NOTION_API_KEY,
-            });
-            const myDatabase = await notion.databases.query({
-                database_id: process.env.NOTION_DB_ID as string,
-            });
-            const database = myDatabase.results.map((page) => {
-                if (!('properties' in page)) return;
-                const { id, created_time: createdTime, properties, url } = page;
-                return {
-                    id,
-                    createdTime,
-                    tag: properties['태그'],
-                    name: properties['이름'],
-                    url,
-                };
-            });
-            setDatabase(database);
+            const res = await fetch('/api/notion');
+            const result = await res.json();
+            setDatabase(result);
         })();
     }, []);
 
