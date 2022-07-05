1653829e125de666ee9b5908fb7a13e91ec5f55e
TaeYoung Kim <taezero@twinny.ai>
2022-07-05 16:16:23 +0900

    Change revalidateGET host to absolute

--- services/revalidate.ts
+++ services/revalidate.ts
@@ -3,7 +3,7 @@ interface RevalidateGET {
 }
 export const revalidateGET = async ({ id }: RevalidateGET) => {
     return await fetch(
-        `api/revalidate?${new URLSearchParams({
+        `/api/revalidate?${new URLSearchParams({
             secret: process.env.NEXT_PUBLIC_REVALIDATE_KEY as string,
             ...(id && { id: id as string }),
         })}`
