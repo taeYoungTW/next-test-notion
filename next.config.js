ca6f0957a396ba4d94a836c8c60ce6e3fd3dff62
RaccoonCode96 <waawoo13@gmail.com>
2022-09-08 22:28:32 +0900

    add rewrite settings for proxy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: ['dummyjson.com'],
-    },
+	reactStrictMode: true,
+	images: {
+		domains: ['dummyjson.com'],
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/og',
+				destination: 'https://leeporter.tistory.com/19',
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
-	return <div>Home.</div>;
+	const [title, setTitle] = useState('wait');
+	useEffect(() => {
+		fetch('/og')
+			.then((res) => res.text())
+			.then((text) => {
+				const HTMLDoc = new DOMParser().parseFromString(text, 'text/html');
+				console.log(HTMLDoc);
+				const ogTitle = HTMLDoc.querySelector(
+					'meta[property="og:title"]'
+				)?.getAttribute('content');
+				ogTitle && setTitle(ogTitle);
+			});
+	}, []);
+	return (
+		<div>
+			<div>Home</div>
+			<div>{title}</div>
+		</div>
+	);
 };
 
 export default Home;
